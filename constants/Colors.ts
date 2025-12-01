199f997ae2eb0a31f566221b686d852a438c1530
zohaibbaig144 <zohaibbaig144@gmail.com>
2025-12-01 23:28:42 +0000

    fix: Resolve app crash on startup

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,16 +1,40 @@
 export const Colors = {
-  light: {
-    text: '#000',
-    background: '#fff',
-    tint: '#2f95dc',
-    tabIconDefault: '#ccc',
-    tabIconSelected: '#2f95dc',
-  },
-  dark: {
-    text: '#fff',
-    background: '#000',
-    tint: '#fff',
-    tabIconDefault: '#ccc',
-    tabIconSelected: '#fff',
-  },
+    light: {
+        text: '#000',
+        background: '#fff',
+        tint: '#2f95dc',
+        tabIconDefault: '#ccc',
+        tabIconSelected: '#2f95dc',
+        card: '#f0f0f0',
+        primary: '#2f95dc',
+        // Add the following properties to conform to the Navigation theme
+        dark: false,
+        colors: {
+            primary: '#2f95dc',
+            background: '#fff',
+            card: '#f0f0f0',
+            text: '#000',
+            border: '#ccc',
+            notification: '#ff0000',
+        },
+    },
+    dark: {
+        text: '#fff',
+        background: '#000',
+        tint: '#fff',
+        tabIconDefault: '#ccc',
+        tabIconSelected: '#fff',
+        card: '#1C1C1E',
+        primary: '#c2ffc2',
+        // Add the following properties to conform to the Navigation theme
+        dark: true,
+        colors: {
+            primary: '#c2ffc2',
+            background: '#000',
+            card: '#1C1C1E',
+            text: '#fff',
+            border: '#333',
+            notification: '#ff0000',
+        },
+    },
 };
