e6a04abfc993b18b628a4d6e6a1433946a4724a2
Zohaib8090 <zohaibbaig144@gmail.com>
2025-12-02 20:33:16 +0500

    Update _layout.tsx

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -1,23 +1,22 @@
 import { Drawer } from 'expo-router/drawer';
 import CustomDrawerContent from './drawer';
-import { ThemeProvider } from '@/hooks/useTheme';
+import { useNotifications } from '@/hooks/useNotifications';
 
 const DrawerLayout = () => {
+  useNotifications();
   return (
-    <ThemeProvider>
-      <Drawer
-        drawerContent={(props) => <CustomDrawerContent {...props} />}
-        screenOptions={({ route }) => ({
-          headerShown: false,
-          drawerActiveTintColor: '#fff',
-          drawerInactiveTintColor: '#fff',
-          drawerItemStyle:
-            route.name === '(tabs)' || route.name === 'drawer'
-              ? { display: 'none' }
-              : {},
-        })}
-      />
-    </ThemeProvider>
+    <Drawer
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        drawerActiveTintColor: '#fff',
+        drawerInactiveTintColor: '#fff',
+        drawerItemStyle:
+          route.name === '(tabs)' || route.name === 'drawer'
+            ? { display: 'none' }
+            : {},
+      })}
+    />
   );
 };
 
