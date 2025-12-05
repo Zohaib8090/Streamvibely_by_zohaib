9d56430b5f0fbd3f8e55a9802aa64b5ea10e0200
zohaibbaig144 <zohaibbaig144@gmail.com>
2025-12-05 01:25:40 +0000

    fix: remove MiniPlayer from drawer layout

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -2,30 +2,22 @@ import 'react-native-gesture-handler';
 import { Drawer } from 'expo-router/drawer';
 import CustomDrawerContent from './drawer';
 import { useNotifications } from '@/hooks/useNotifications';
-import { PlayerProvider } from '../../contexts/PlayerContext';
-import MiniPlayer from '../../components/MiniPlayer';
-import { View } from 'react-native';
 
 const DrawerLayout = () => {
   useNotifications();
   return (
-    <PlayerProvider>
-      <View style={{ flex: 1 }}>
-        <Drawer
-          drawerContent={(props) => <CustomDrawerContent {...props} />}
-          screenOptions={({ route }) => ({
-            headerShown: false,
-            drawerActiveTintColor: '#fff',
-            drawerInactiveTintColor: '#fff',
-            drawerItemStyle:
-              route.name === '(tabs)' || route.name === 'drawer'
-                ? { display: 'none' }
-                : {},
-          })}
-        />
-        <MiniPlayer />
-      </View>
-    </PlayerProvider>
+    <Drawer
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        drawerActiveTintColor: '#fff',
+        drawerInactiveTintColor: '#fff',
+        drawerItemStyle:
+          route.name === '(tabs)' || route.name === 'drawer'
+            ? { display: 'none' }
+            : {},
+      })}
+    />
   );
 };
 
